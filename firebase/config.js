933760d9b4f9ac3672702c40ddf7a39cc9f9072a
Mrudul Kolambe <83169198+mrudulkolambe@users.noreply.github.com>
2021-07-29 20:25:43 +0530

    Create config.js

---
+++ firebase/config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
